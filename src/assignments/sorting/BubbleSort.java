04819fccf0caffc1ff12226049e31fa526b47f65
umolina-hcc <umolina@hcc.nd.edu>
2026-08-31 08:57:52 -0400

    git commit

---
+++ src/assignments/sorting/BubbleSort.java
@@ -0,0 +1,48 @@
+package assignments.sorting;
+/**
+ * 
+ * Swap adjacent elements over and over until the whole array is sorted.
+ */
+public class BubbleSort extends SortingAlgorithm {
+    /**
+     * Sort an array in-place using bubble sort
+     * 
+     * Post-condition: arrary is sorted in ascending order
+     * 
+     * @param array an arrary of integers
+     * 
+     * 
+     */
+    public void sort(Integer[] array) {
+        for (int k = array.length; k >= 2; k --) {    // k is the length of the sub-array we are looping through in the inner loop
+            for (int i = 0; i < k; i++) {
+                if (array[i] > array[i+1]) {
+                    // swap adjacent items (i and i+1)
+                    swap(array, i, i+1);
+                }
+            }
+        }
+    }
+    /**
+     * swap two elements within an array
+     * @param array the array to swap values in
+     * @param i the first index to swap
+     * @param j the second index to swap
+     */
+    private void swap(Integer[] array, int i, int j) {
+        Integer temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    /**
+     * 
+     * @param args command-line args
+     */
+    public static void main(String[] args) {
+        SortingAlgorithm.validate(new BubbleSort());
+        System.out.println("BubbleSort has passed all tests.");
+    }
+
+}
