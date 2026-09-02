372db9d2f853998019837028e85b012c6fc6a89e
umolina-hcc <umolina@hcc.nd.edu>
2026-09-02 16:34:59 -0400

    git commit

--- src/assignments/sorting/BubbleSort.java
+++ src/assignments/sorting/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSort extends SortingAlgorithm {
      */
     public void sort(Integer[] array) {
         for (int k = array.length; k >= 2; k --) {    // k is the length of the sub-array we are looping through in the inner loop
-            for (int i = 0; i < k-1; i++) { //go up to k-1 bec
+            for (int i = 0; i < k-1; i++) { //go up to k-1 to prevent out of bounds error
                 if (array[i] > array[i+1]) {
                     // swap adjacent items (i and i+1)
                     swap(array, i, i+1);
--- src/assignments/sorting/InsertionSort.java
+++ src/assignments/sorting/InsertionSort.java
@@ -1,22 +1,35 @@
-// package assignments.sorting;
-// /** 
-//  * Putting elements one by one into a guaranteed-sorted list
-//  * 
-//  */
+package assignments.sorting;
+/** 
+ * Putting elements one by one into a guaranteed-sorted list
+ * 
+ */
 
-// public class InsertionSort extends SortingAlgorithm {
-//     /**
-//      * 
-//      * Sort an array using InsertionSort
-//      * 
-//      * Post-condition: Array is sorted in ascending order
-//      * 
-//      */
-//     public void sort(Integer[] array) {
-//         for (int k = array.length; k = 1; k--) 
-//             for (int = 0; i < k -1; i++) {
-//                 if (array[i] > )
-//         }
-//     }
-// }
-// }
+public class InsertionSort extends SortingAlgorithm {
+    /**
+     * Sort an array using InsertionSort
+     * 
+     * Post-condition: Array is sorted in ascending order
+     * 
+     * @param array an array of integers
+     * 
+     */
+    public void sort(Integer[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int currentValue = array[i];
+
+            int j = i-1;
+            while (j >= 0 && array[j] > currentValue) {
+                array[j + 1] = array[j];
+                j = j--;
+            }
+            array[j + 1] = currentValue;
+        } 
+    }
+
+
+    public static void main(String[] args) {
+        SortingAlgorithm.validate(new BubbleSort());
+        System.out.println("InsertionSort has passed all tests.");
+    }
+}
+    
